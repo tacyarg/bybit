4fd3a27005c12d82b3871a4f1aca40dcd94c7bde
Jacob Dent <jdent@tacyarg.com>
2019-08-29 14:57:15 -0500

    init

---
+++ client.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch')
+const crypto = require('crypto')
+const qs = require('qs')
+const assert = require('assert')
+
+module.exports = ({ key, secret }) => {
+  assert(key, 'api key required.')
+  assert(secret, 'api secret required.')
+
+  const call = function(verb, endpoint, params) {
+
+    const options = {
+      //TODO: prepare request
+    }
+
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(
+        response => {
+          if ('error' in response) throw new Error(response.error.message)
+          return response
+        },
+        error => console.error('Network error', error)
+      )
+  }
+
+  call.post = (endpoint, params) => {
+    return call('POST', endpoint, params)
+  }
+
+  call.get = (endpoint, params) => {
+    return call('GET', endpoint, params)
+  }
+
+  return {
+    listOpenPositions() {
+      return call.get('position', {
+        filter: { symbol: 'XBTUSD' },
+        columns: ['currentQty', 'avgEntryPrice'],
+      })
+    },
+    createLimitOrder(qty, price) {
+      return call.post('order', {
+        symbol: 'XBTUSD',
+        orderQty: qty,
+        price: price,
+      })
+    },
+    createStopOrder(qty, price) {
+      return call.post('order', {
+        symbol: 'XBTUSD',
+        orderQty: qty,
+        stopPx: price,
+      })
+    },
+  }
+}
